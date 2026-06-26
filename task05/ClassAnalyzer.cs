a627797bbed064e56ab0aaef4f5df6f7c0da7e65
Nastya <essen.nastya@gmail.com>
2026-06-26 10:55:44 +0600

    Add ClassAnalyzer

---
+++ task05/ClassAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+namespace task05;
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+    public IEnumerable<string> GetPublicMethods()
+    {
+     return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                .Select(m => m.Name);
+    }
+    public IEnumerable<string> GetMethodParams(string methodName)
+    {
+        var method = _type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+        if (method == null)
+        {
+            return Enumerable.Empty<string>();
+        }
+        var param = method.GetParameters().Select(p => p.Name);
+        var type = new[] { method.ReturnType.Name };
+        return param.Concat(type);
+    }
+    public IEnumerable<string> GetAllFields()
+    {
+        return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(f => f.Name);
+    }
+    public IEnumerable<string> GetProperties()
+    {
+        return _type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)  
+                    .Select(p => p.Name);
+    }
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return _type.GetCustomAttributes(typeof(T), inherit: true).Any();
+    }
+}
---
+++ task05tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using task05;
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField;
+    public int Property { get; set; }
+
+    public void Method() { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+    [Fact]
+    public void GetProperties_ReturnsPropertyNames()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+
+        Assert.Contains("Property", properties);
+    }
+    [Fact]
+    public void HasAttribute_ReturnsTrueWhenAttributeExists()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+
+        Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+    }
+}
