59e87264068c473622d6a298af8e9509767e4031
Nastya <essen.nastya@gmail.com>
2026-07-03 14:26:01 +0600

    Add ICommand, команды для работы с файловой системой, загрузка команд из DLL

---
+++ task08/FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,23 @@
+ using System.IO;
+using System.Collections.Generic;
+using CommandLib;
+
+namespace FileSystemCommands;
+
+public class FindFilesCommand : ICommand
+{
+    private readonly string _path;
+    private readonly string _pattern;
+    public List<string> FoundFiles { get; private set; } = new();
+
+    public FindFilesCommand(string path, string pattern)
+    {
+        _path = path;
+        _pattern = pattern;
+    }
+
+    public void Execute()
+    {
+        FoundFiles = Directory.EnumerateFiles(_path, _pattern, SearchOption.AllDirectories).ToList();
+    }
+}
