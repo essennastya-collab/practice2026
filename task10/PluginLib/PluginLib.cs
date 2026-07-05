6397a1d09a05290162ea05c244c249720155ee97
Nastya <essen.nastya@gmail.com>
2026-07-05 13:02:32 +0600

    add plugins

---
+++ task10/PluginLib/PluginLib.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace PluginLib;
+
+public interface IPlugin
+{
+    void Execute();
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class PluginLoadAttribute : Attribute
+{
+    public string[] Dependencies { get; set; } = Array.Empty<string>();
+}
+
+public class PluginManager
+{
+    public class PluginHost
+    {
+        private readonly List<Type> _plugins = new();
+
+        public IReadOnlyList<Type> Plugins => _plugins.AsReadOnly();
+
+        public void LoadFromDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+                throw new DirectoryNotFoundException($"Directory not found: {directoryPath}");
+
+            var dllFiles = Directory.GetFiles(directoryPath, "*.dll");
+            
+            foreach (var dllFile in dllFiles)
+            {
+                var assembly = Assembly.LoadFrom(dllFile);
+                var pluginTypes = from type in assembly.GetTypes()
+                                  where type.GetCustomAttribute<PluginLoadAttribute>() != null
+                                  where typeof(IPlugin).IsAssignableFrom(type)
+                                  where !type.IsAbstract
+                                  select type;
+
+                _plugins.AddRange(pluginTypes);
+            }
+        }
+
+        public void LoadFromTypes(IEnumerable<Type> types)
+        {
+            var validPlugins = from type in types
+                               where type.GetCustomAttribute<PluginLoadAttribute>() != null
+                               where typeof(IPlugin).IsAssignableFrom(type)
+                               where !type.IsAbstract
+                               select type;
+
+            _plugins.AddRange(validPlugins);
+        }
+
+        public void ExecuteAll()
+        {
+            var sortedPlugins = GetTopologicalOrder();
+            
+            foreach (var pluginType in sortedPlugins)
+            {
+                var instance = (IPlugin)Activator.CreateInstance(pluginType)!;
+                instance.Execute();
+            }
+        }
+
+        private List<Type> GetTopologicalOrder()
+        {
+            var result = new List<Type>();
+            var processed = new HashSet<Type>();
+
+            foreach (var plugin in _plugins)
+            {
+                ProcessDependencies(plugin, processed, result);
+            }
+
+            return result;
+        }
+
+        private void ProcessDependencies(Type pluginType, HashSet<Type> processed, List<Type> result)
+        {
+            if (!processed.Add(pluginType))
+                return;
+
+            var attribute = pluginType.GetCustomAttribute<PluginLoadAttribute>();
+            
+            if (attribute?.Dependencies != null)
+            {
+                foreach (var dependencyName in attribute.Dependencies)
+                {
+                    var dependency = _plugins.FirstOrDefault(p => p.Name == dependencyName);
+                    
+                    if (dependency != null)
+                    {
+                        ProcessDependencies(dependency, processed, result);
+                    }
+                }
+            }
+
+            result.Add(pluginType);
+        }
+    }
+}
---
+++ task10tests/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using PluginLib;
+
+namespace task10tests;
+
+public class UnitTest1
+{
+    public static class ExecutionLog
+    {
+        public static List<string> Sequence = new();
+    }
+
+    [PluginLoad]
+    public class BasePlugin : IPlugin
+    {
+        public void Execute() => ExecutionLog.Sequence.Add("Base");
+    }
+
+    [PluginLoad(Dependencies = new[] { "BasePlugin" })]
+    public class MiddlePlugin : IPlugin
+    {
+        public void Execute() => ExecutionLog.Sequence.Add("Middle");
+    }
+
+    [PluginLoad(Dependencies = new[] { "MiddlePlugin" })]
+    public class FinalPlugin : IPlugin
+    {
+        public void Execute() => ExecutionLog.Sequence.Add("Final");
+    }
+
+    public class PluginHostTests
+    {
+        [Fact]
+        public void ExecutionOrder_ShouldRespectDependencyChain()
+        {
+            ExecutionLog.Sequence.Clear();
+            var host = new PluginManager.PluginHost();
+            var assemblyDir = Path.GetDirectoryName(typeof(BasePlugin).Assembly.Location)!;
+
+            host.LoadFromDirectory(assemblyDir);
+            host.ExecuteAll();
+
+            Assert.Equal(new[] { "Base", "Middle", "Final" }, ExecutionLog.Sequence);
+        }
+
+        [Fact]
+        public void EmptyDirectory_ShouldExecuteWithoutExceptions()
+        {
+            var tempDir = Path.Combine(Path.GetTempPath(), $"PluginTest_{Guid.NewGuid()}");
+            Directory.CreateDirectory(tempDir);
+
+            var host = new PluginManager.PluginHost();
+            
+            var exception = Record.Exception(() =>
+            {
+                host.LoadFromDirectory(tempDir);
+                host.ExecuteAll();
+            });
+
+            Directory.Delete(tempDir, true);
+
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void LoadFromTypes_ShouldPreserveInsertionOrderWhenNoDeps()
+        {
+            var host = new PluginManager.PluginHost();
+            host.LoadFromTypes(new[] { typeof(BasePlugin), typeof(MiddlePlugin), typeof(FinalPlugin) });
+
+            Assert.Equal(3, host.Plugins.Count);
+            Assert.Contains(typeof(BasePlugin), host.Plugins);
+            Assert.Contains(typeof(MiddlePlugin), host.Plugins);
+            Assert.Contains(typeof(FinalPlugin), host.Plugins);
+        }
+    }
+}
