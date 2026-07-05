973cb19f5257277a69041c44c70847677629d3ef
Nastya <essen.nastya@gmail.com>
2026-07-05 13:37:08 +0600

    Add Calculator

---
+++ task11/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+namespace Task11;
+
+public interface ICalculator
+{
+    int Add(int a, int b);
+    int Minus(int a, int b);
+    int Mul(int a, int b);
+    int Div(int a, int b);
+}
