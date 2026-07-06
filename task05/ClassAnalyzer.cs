6c281aca55dfd76040ac0972bc9526541a6c7524
Nastya <essen.nastya@gmail.com>
2026-07-06 15:12:20 +0600

    update

--- task05/ClassAnalyzer.cs
+++ task05/ClassAnalyzer.cs
@@ -2,43 +2,60 @@
 using System.Reflection;
 using System.Linq;
 using System.Collections.Generic;
+
 namespace task05;
+
 public class ClassAnalyzer
 {
-    private Type _type;
+    private readonly Type _type;
 
     public ClassAnalyzer(Type type)
     {
+        if (type == null)
+        {
+            throw new ArgumentNullException(nameof(type), "Тип не может быть null");
+        }
         _type = type;
     }
+
     public IEnumerable<string> GetPublicMethods()
     {
-     return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly)
-                .Select(m => m.Name);
+        return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                    .Select(m => m.Name);
     }
+
     public IEnumerable<string> GetMethodParams(string methodName)
     {
+        if (methodName == null)
+        {
+            throw new ArgumentNullException(nameof(methodName), "Имя метода не может быть null");
+        }
+
         var method = _type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
         if (method == null)
         {
             return Enumerable.Empty<string>();
         }
+
         var param = method.GetParameters().Select(p => p.Name);
-        var type = new[] { method.ReturnType.Name };
-        return param.Concat(type);
+        var returnType = new[] { method.ReturnType.Name };
+        return param.Concat(returnType);
     }
+
     public IEnumerable<string> GetAllFields()
     {
         return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
                     .Select(f => f.Name);
     }
+
     public IEnumerable<string> GetProperties()
     {
-        return _type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)  
+        return _type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
                     .Select(p => p.Name);
     }
+
     public bool HasAttribute<T>() where T : Attribute
     {
-        return _type.GetCustomAttributes(typeof(T), inherit: true).Any();
+        return _type.IsDefined(typeof(T), inherit: true);
     }
 }
