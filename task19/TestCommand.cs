dbdcb10b3ceaeda4efe94c6c0a0e85f6a6772f9c
Nastya <essen.nastya@gmail.com>
2026-07-16 14:45:51 +0600

    feat: illustrate pseudo-parallel execution with TestCommand and HardStop

---
+++ task19/TestCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using task17;
+
+namespace task19;
+
+public class TestCommand : ILongRunningCommand
+{
+    private readonly int _id;
+    private int _counter = 0;
+    private const int MaxExecutions = 3;
+    private readonly Action<string> _log;
+
+    public TestCommand(int id, Action<string> log = null)
+    {
+        _id = id;
+        _log = log ?? Console.WriteLine;
+    }
+
+    public bool IsCompleted => _counter >= MaxExecutions;
+
+    public void Execute()
+    {
+        if (IsCompleted) return;
+        
+        _counter++;
+        _log($"Поток {_id} вызов {_counter}");
+        
+        Thread.Sleep(10); 
+    }
+}
